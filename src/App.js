a86f4ac73556453c2880818b080f228cb60752cc
rjvirus <rahulvirus100@gmail.com>
2021-01-24 22:34:45 +0100

    modify page limit options

--- src/App.js
+++ src/App.js
@@ -138,14 +138,12 @@ function App() {
                   <select name="limit" id="limit" value={pageLimit.toString()} onChange={(e) => {
                     setPageLimit(parseInt(e.target.value))
                   }}>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
                     <option value="3">3</option>
                     <option value="4">4</option>
-                    <option value="5">5</option>
                     <option value="6">6</option>
-                    <option value="7">7</option>
                     <option value="8">8</option>
+                    <option value="8">10</option>
+                    <option value="8">15</option>
                   </select>
                 </div>
               </div>
